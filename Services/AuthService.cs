b5c1a84e7731dabe7ace153f1446cf7f4b5adcc4
Yusuf-Sefa-Laptop <yusufsefaartar@hotmail.com>
2026-07-22 20:01:52 +0300

    AuthService updated

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,11 +1,16 @@
 
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ECommerceAPI.Dtos.TokenDtos;
 using ECommerceAPI.Dtos.UserDtos;
 using ECommerceAPI.Entities;
+using ECommerceAPI.Entities.Enums;
 using ECommerceAPI.Repository.RepositoryInterfaces;
+using ECommerceAPI.Services.Interfaces;
 using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +18,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace ECommerceAPI.Services;
 
-public class AuthService
+public class AuthService : IAuthService
 {
     private readonly IEnumerableRepository<User> E_repository;
     private readonly IQueryableRepository<User> Q_repository;
@@ -21,13 +26,16 @@ public class AuthService
     private readonly IValidator<LoginUserDto> _loginValidator;
     private readonly IMapper _mapper;
     private readonly IConfiguration configuration;
-    public AuthService(User user,
-                        IEnumerableRepository<User> E_repository,
+    private readonly IPasswordHasher<User>  _passwordHasher;
+
+
+    public AuthService(IEnumerableRepository<User> E_repository,
                         IQueryableRepository<User> Q_repository,
                         IValidator<AuthUserDto> _authValidator,
                         IValidator<LoginUserDto> _loginValidator,
                         IMapper _mapper,
-                        IConfiguration configuration)
+                        IConfiguration configuration,
+                        IPasswordHasher<User> _passwordHasher)
     {
         this.E_repository = E_repository;
         this.Q_repository = Q_repository;
@@ -35,25 +43,34 @@ public class AuthService
         this._loginValidator = _loginValidator;
         this._mapper = _mapper;
         this.configuration = configuration;
+        this._passwordHasher = _passwordHasher;
     }
 
-    public async Task<ResponseUserDto> Register (AuthUserDto request)
+    public async Task<ResponseUserDto?> RegisterAsync (AuthUserDto request)
     {
         var validationResponse = await _authValidator.ValidateAsync(request);
         if (!validationResponse.IsValid)
             throw new ValidationException(validationResponse.Errors.ToString());
 
+        if(await Q_repository.Q_GetAll().AnyAsync(u => u.Email == request.Email))
+            return null;
+
         var userEntity = _mapper.Map<User>(request);
 
-        var hashedPassword = new PasswordHasher<User>()
-                                    .HashPassword(userEntity, request.Password);
+        var hashedPassword = _passwordHasher.HashPassword(userEntity, request.Password);
 
         userEntity.Password = hashedPassword;
+        userEntity.UserType = UserTypes.Customer;
 
-        return _mapper.Map<ResponseUserDto>(userEntity);
+        await E_repository.E_AddEntity(userEntity);
+
+        //return _mapper.Map<ResponseUserDto>(userEntity);
+        return await Q_repository
+                        .Q_GetByFilter(u => u.Id == userEntity.Id)
+                        .ProjectTo<ResponseUserDto>(_mapper.ConfigurationProvider)
+                        .FirstOrDefaultAsync();
     }
-
-    public async Task<string> Login (LoginUserDto request)
+    public async Task<ResponseTokenDto?> LoginAsync (LoginUserDto request)
     {
         var validationResponse = await _loginValidator.ValidateAsync(request);
         if(!validationResponse.IsValid)
@@ -62,18 +79,59 @@ public class AuthService
         var user = await Q_repository.Q_GetByFilter(u => u.Email == request.Email).FirstOrDefaultAsync();
 
         if(user is null)
-            throw new Exception("Wrong e-mail or password");
+            return null;
             
-        if(new PasswordHasher<User>().VerifyHashedPassword(user, user.Password, request.Password)
+        if(_passwordHasher.VerifyHashedPassword(user, user.Password, request.Password)
             == PasswordVerificationResult.Failed)
-            throw new Exception("Wrong e-mail or password");
+            return null;
 
-        string token = CreateToken(user);
+        return await CreateResponseToken(user);
+    }
+    
+    public async Task<ResponseTokenDto?> RefreshTokenAsync(ResponseRefreshTokenDto request)
+    {
+        var user = await ValidateRefreshTokenAsync(request.UserId, request.RefreshToken);
+        
+        return user is null
+                ? null
+                : await CreateResponseToken(user);
+    }
+    private async Task<User?> ValidateRefreshTokenAsync(int id, string refreshToken)
+    {
+        var user = await Q_repository.Q_GetWithInclude(u => u.Id == id).FirstOrDefaultAsync();
+        if(user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+            return null;
 
-        return token;
+        return user;
+    }
+    private string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+    private async Task<string> GenerateAndSaveRefreshTokenAsync(User user)
+    {
+        var refreshToken = GenerateRefreshToken();
+        await Q_repository.Q_GetByFilter(u => u.Id == user.Id)
+                            .ExecuteUpdateAsync(u => u
+                                                    .SetProperty(u => u.RefreshToken, refreshToken)
+                                                    .SetProperty(u => u.RefreshTokenExpiryTime, DateTime.UtcNow.AddDays(7))
+                                                );
+        
+        return refreshToken;
+    }
+    private async Task<ResponseTokenDto> CreateResponseToken(User? user)
+    {
+        return new ResponseTokenDto
+        {
+            AccessToken = CreateToken(user),
+            Refreshtoken = await GenerateAndSaveRefreshTokenAsync(user)
+        };
     }
 
-    public string CreateToken(User user)
+    private string CreateToken(User user)
     {
         var claims = new List<Claim>
         {
